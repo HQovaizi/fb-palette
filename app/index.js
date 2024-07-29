300e8ca90bc92e0698d5310dc4f2793b1928e920
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2024-07-29 19:48:27 -0400

    Update Divider

--- app/index.js
+++ app/index.js
@@ -5,21 +5,21 @@ const applicationFill = document.getElementById("application-fill");
 let tumblerHour = document.getElementById("tumbler-hour");
 let tumblerMins = document.getElementById("tumbler-mins");
 
-let units = document.getElementById("units");
-let AM = units.getElementById("tumbler-toggle-top");
-let PM = units.getElementById("tumbler-toggle-bot");
+// let units = document.getElementById("units");
+// let AM = units.getElementById("tumbler-toggle-top");
+// let PM = units.getElementById("tumbler-toggle-bot");
 
-unitToggle("AM");
+// unitToggle("AM");
 
-function unitToggle(unit) {
-  if (unit === "AM") {
-    AM.style.fillOpacity = 1;
-    PM.style.fillOpacity = 0.3;
-  } else {
-    AM.style.fillOpacity = 0.3;
-    PM.style.fillOpacity = 1;
-  }
-}
+// function unitToggle(unit) {
+//   if (unit === "AM") {
+//     AM.style.fillOpacity = 1;
+//     PM.style.fillOpacity = 0.3;
+//   } else {
+//     AM.style.fillOpacity = 0.3;
+//     PM.style.fillOpacity = 1;
+//   }
+// }
 
 tumblerHour.addEventListener("select", (evt) => {
   let selectedIndex1 = parseInt(tumblerHour.value);
