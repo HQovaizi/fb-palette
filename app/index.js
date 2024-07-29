97b10090b5804636d4f05a49692f23bf62e03b04
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2024-07-29 19:16:57 -0400

    Replace Numbers With Hex

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 import document from "document";
 
+const applicationFill = document.getElementById("application-fill");
+
 let tumblerHour = document.getElementById("tumbler-hour");
 let tumblerMins = document.getElementById("tumbler-mins");
 
@@ -20,21 +22,35 @@ function unitToggle(unit) {
 }
 
 tumblerHour.addEventListener("select", (evt) => {
-  let selectedIndex = parseInt(tumblerHour.value);
-  let selectedItem = tumblerHour.getElementById(`hour-item${selectedIndex}`);
-  let selectedValue = selectedItem.getElementById("text").text;
-  console.log(`HOUR: index: ${selectedIndex} :: value: ${selectedValue}`);
+  let selectedIndex1 = parseInt(tumblerHour.value);
+  let selectedItem1 = tumblerHour.getElementById(`hour-item${selectedIndex1}`);
+  let selectedValue1 = selectedItem1.getElementById("text").text;
+  console.log(`HOUR: index: ${selectedIndex1} :: value: ${selectedValue1}`);
 
-  if (selectedIndex >= 12) {
-    unitToggle("PM");
-  } else {
-    unitToggle("AM");
-  }
+  let selectedIndex2 = tumblerMins.value;
+  let selectedItem2 = tumblerMins.getElementById(`min-item${selectedIndex2}`);
+  let selectedValue2 = selectedItem2.getElementById("text").text;
+  console.log(`MINS: index: ${selectedIndex2} :: value: ${selectedValue2}`);
+
+  applicationFill.style.fill = `#${selectedValue1}${selectedValue2}0000`;
+
+  // if (selectedIndex >= 12) {
+  //   unitToggle("PM");
+  // } else {
+  //   unitToggle("AM");
+  // }
 });
 
 tumblerMins.addEventListener("select", (evt) => {
-  let selectedIndex = tumblerMins.value;
-  let selectedItem = tumblerMins.getElementById(`min-item${selectedIndex}`);
-  let selectedValue = selectedItem.getElementById("text").text;
-  console.log(`MINS: index: ${selectedIndex} :: value: ${selectedValue}`);
+  let selectedIndex1 = parseInt(tumblerHour.value);
+  let selectedItem1 = tumblerHour.getElementById(`hour-item${selectedIndex1}`);
+  let selectedValue1 = selectedItem1.getElementById("text").text;
+  console.log(`HOUR: index: ${selectedIndex1} :: value: ${selectedValue1}`);
+
+  let selectedIndex2 = tumblerMins.value;
+  let selectedItem2 = tumblerMins.getElementById(`min-item${selectedIndex2}`);
+  let selectedValue2 = selectedItem2.getElementById("text").text;
+  console.log(`MINS: index: ${selectedIndex2} :: value: ${selectedValue2}`);
+  
+  applicationFill.style.fill = `#${selectedValue1}${selectedValue2}0000`;
 });
